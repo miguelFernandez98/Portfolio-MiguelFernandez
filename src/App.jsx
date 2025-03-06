ab918d86da23d3a0ab1092f9fcfbdf49a5d6af42
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-03-06 16:39:46 -0400

    `Added Navbar component and updated App component layout`

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
-
+import { Navbar } from "./components/Navbar";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <section
+        className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}
+      >
+        <Navbar />
+      </section>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+      <section className=" max-w-xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+            <a href="" className="font-mono">
+                
+            </a>
+        </div>
+      </section>
+    </nav>
+  );
+};
