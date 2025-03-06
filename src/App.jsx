247daabca04bd74b7a27229337208d812ec126f9
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-03-06 15:52:05 -0400

    `feat: Add loading screen, preconnect to Google Fonts, and Tailwind CSS, and update CSS and JSX files`

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <>
-    </>
-  )
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+  );
 }
 
-export default App
+export default App;
