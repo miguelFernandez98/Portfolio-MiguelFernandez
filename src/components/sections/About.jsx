5ffcff1f5b0edd31931f528941a0dd744abe54ab
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-03-11 14:55:03 -0400

    `Added new sections to About page for education and work experience`

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -15,7 +15,7 @@ export const About = () => {
       id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
-      <div className="max-w-3-xl mx-auto px-4">
+      <div className="max-w-3xl mx-auto px-4">
         <h2 className=" text-3xl font-bold mb-8 bg-gradient-to-br from-blue-500 to-emerald-600 text-center bg-clip-text text-transparent">
           {" "}
           About Me
@@ -25,8 +25,8 @@ export const About = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iure
             sed facere doloribus! Hic delectus repellat voluptatibus excepturi
           </p>
-          <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className=" rounded-3xl p-6 hover:-translate-y-1 transition-all">
+          <section className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+            <div className="rounded-3xl p-6 hover:-translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4">Frontend</h3>
               <div className="flex flex-wrap gap-2">
                 {frontEndSkills.map((tech, key) => (
@@ -53,6 +53,57 @@ export const About = () => {
               </div>
             </div>
           </section>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">Education</h3>
+            <ul className="list-disc list-inside2 text-gray-300 space-y-2">
+              <li>
+                <strong>Systems engineer</strong> - Universidad de Margarita
+                (2017-2022)
+              </li>
+              {/* <li>
+                <strong>Systems engineer</strong> - Universidad de Margarita
+             (2017-2022)
+              </li> */}
+            </ul>
+          </div>
+          <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+            <h3 className="text-xl font-bold mb-4">Work Experience</h3>
+            <div className="space-y-4 text-gray-300">
+              <div>
+                <h4 className="font-semibold">
+                  Frontend developer at OMNINEXO - WAIKERI HITECH (2024 -
+                  Present)
+                </h4>
+                <p>
+                  {" "}
+                  Developing mobile and web applications to optimize business
+                  management and operations.{" "}
+                </p>
+              </div>
+              <div>
+                <h4 className="font-semibold">
+                  Development specialist at INSIDE MARKET/Rio supermarket (2022
+                  - 2024)
+                </h4>
+                <p>
+                  {" "}
+                  Developed applications to enhance company efficiency and user
+                  experience, including scheduling and reconciliation
+                  tools.{" "}
+                </p>
+              </div>
+              {/* <div>
+                <h4>Development analyst at GUUAO/LEIROS (2023)</h4>
+                <p>
+                  {" "}
+                  Improved an outdated mobile app by resolving errors and
+                  enhancing functionality through design changes.{" "}
+                </p>
+              </div> */}
+            </div>
+          </div>
         </div>
       </div>
     </section>
