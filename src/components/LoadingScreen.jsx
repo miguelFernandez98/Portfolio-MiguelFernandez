57571b995f6360a07a4837d8d460a163e1a59a34
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-03-12 10:22:26 -0400

    `Added RevealOnScroll component to sections/About.jsx, sections/Home.jsx, and sections/Projects.jsx`

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -18,17 +18,20 @@ export const LoadingScreen = ({ onComplete }) => {
     return () => clearInterval(interval);
   }, [onComplete]);
 
+  //remove scroll in LoadingScreen
+  useEffect(() => {
+    document.body.classList.add("no-scroll");
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, []);
+
   return (
     <section className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
       <div className="mb-4 text-4xl font-mono font-bold">
         {text}
         <span className="animate-blink ml-1">|</span>
       </div>
-     {/*  <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar">
-          {""}
-        </div>
-      </div> */}
     </section>
   );
 };
