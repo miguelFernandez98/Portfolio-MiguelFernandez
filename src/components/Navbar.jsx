8e280f55746cf431bdb71652d51e763e2ac34653
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-03-13 11:00:53 -0400

    `Fixed typo in Navbar component: <btn> -> <button>`

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
       <section className="max-w-xl mx-auto px-4 py-2 flex justify-between items-center">
         <div className="px-6">
           <a href="#home" className="font-mono">
-            <img className="h-5 w-auto" src={logo} alt="Logo staff"/>
+            <img className="h-5 w-auto" src={logo} alt="Logo staff" />
           </a>
         </div>
         <div className="flex justify-center items-center h-12 py-3 px-6 md:bg-[rgba(10,10,10,0.8)] md:backdrop-blur-sm md:border md:border-white/20 md:shadow-lg md:rounded-4xl">
@@ -66,7 +66,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
           </div>
         </div>
         <div className="hidden md:flex">
-          <btn id="darkMode" className="font-mono">
+          <button id="darkMode" className="font-mono">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -81,7 +81,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
                 d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
               />
             </svg>
-          </btn>
+          </button>
         </div>
       </section>
     </nav>
