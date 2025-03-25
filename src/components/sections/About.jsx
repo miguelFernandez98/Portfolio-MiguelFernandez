f55517c5fbb326c9071c0508c6d4fe08401b106a
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-03-25 11:03:01 -0400

    `Added work experience section to About component`

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -12,6 +12,30 @@ export const About = () => {
 
   const backEndSkills = ["Node.js", "Express", "MongoDB", "Docker"];
 
+  const workExperience = [
+    {
+      position: "Frontend developer",
+      company: "OMNINEXO - WAIKERI HITECH",
+      period: "2024 - Present",
+      description:
+        "Developing mobile and web applications to optimize business management and operations.",
+    },
+    {
+      position: "Development specialist",
+      company: "INSIDE MARKET/RIO SUPERMARKET",
+      period: "2022 - 2024",
+      description:
+        "Developed applications to enhance company efficiency and user experience, including scheduling and reconciliation tools.",
+    },
+    {
+      position: "Development analyst",
+      company: "GUUAO/LEIROS",
+      period: "2023 - 2023",
+      description:
+        "Improved an outdated mobile app by resolving errors and enhancing functionality through design changes.",
+    },
+  ];
+
   return (
     <section
       id="about"
@@ -19,17 +43,16 @@ export const About = () => {
     >
       <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
-          <h2 className=" text-3xl font-bold mb-8 bg-gradient-to-br from-blue-500 to-emerald-600 text-center bg-clip-text text-transparent">
-            {" "}
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-br from-blue-500 to-emerald-600 text-center bg-clip-text text-transparent">
             About Me
           </h2>
           <article className="rounded-4xl p-8 border-white/10 border hover:-translate-y-0 transition-all">
-            <p className="text-gray-300 mb-b text-center">
+            <p className="text-gray-300 mb-6 text-center">
               Experienced frontend developer with backend basics. Adaptable to
               new technologies and continuous learner.{" "}
               <strong>University of Margarita graduate 2017-2022 </strong>.
             </p>
-            <ul className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+            <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <li className="rounded-4xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4">Frontend</h3>
                 <div className="flex flex-wrap gap-2">
@@ -61,43 +84,28 @@ export const About = () => {
 
           <div className="flex justify-center items-center gap-6 mt-8">
             <article className="p-6 rounded-4xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4">Work Experience</h3>
-              <ul className="space-y-4 text-gray-300">
-                <li>
-                  <h4 className="font-semibold">
-                    Frontend developer <br /> OMNINEXO - WAIKERI HITECH{" "}
-                    <span className="whitespace-nowrap">(2024 - Present)</span>
-                  </h4>
-                  <p>
-                    {" "}
-                    Developing mobile and web applications to optimize business
-                    management and operations.{" "}
-                  </p>
-                </li>
-                <li>
-                  <h4 className="font-semibold">
-                    Development specialist <br />
-                    INSIDE MARKET/RIO SUPERMARKET{" "}
-                    <span className="whitespace-nowrap">(2022 - 2024)</span>
-                  </h4>
-                  <p>
-                    {" "}
-                    Developed applications to enhance company efficiency and
-                    user experience, including scheduling and reconciliation
-                    tools.{" "}
-                  </p>
-                </li>
-                <li>
-                  <h4 className="font-semibold">
-                    Development analyst <br /> GUUAO/LEIROS{" "}
-                    <span className="whitespace-nowrap">(2023 - 2023)</span>
-                  </h4>
-                  <p>
-                    {" "}
-                    Improved an outdated mobile app by resolving errors and
-                    enhancing functionality through design changes.{" "}
-                  </p>
-                </li>
+              <h3 className="text-xl font-bold mb-6">Work Experience</h3>
+              <ul role="list" className="m-4 max-w-screen-md">
+                {workExperience.map((job, index) => (
+                  <li
+                    key={index}
+                    className="group relative flex flex-col pb-8 pl-7 last:pb-0"
+                  >
+                    <div className="absolute bottom-0 left-[calc(0.33rem-0.5px)] top-0 w-px bg-blue-500/20 group-first:top-3" />
+                    <div className="absolute left-0 top-2 h-2.5 w-2.5 rounded-full border-[1.5px] border-blue-500 bg-[#0a0a0a]" />
+                    <h4 className="mt-2 font-semibold text-white">
+                      {job.position}
+                      <br />
+                      <span className="text-gray-400 ">{job.company}</span>
+                    </h4>
+                    <p className="font-display order-first font-semibold tracking-wide text-blue-400">
+                      <time dateTime={job.period.replace(" - ", "/")}>
+                        ({job.period})
+                      </time>
+                    </p>
+                    <p className="mt-2 text-gray-400 ">{job.description}</p>
+                  </li>
+                ))}
               </ul>
             </article>
           </div>
