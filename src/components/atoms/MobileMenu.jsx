2f507a8f06c1403d09d93cc84b2835275a8af0de
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-03-26 11:50:39 -0400

    `Added isDark prop to MobileMenu component and updated Home component to download CV PDF`

--- src/components/atoms/MobileMenu.jsx
+++ src/components/atoms/MobileMenu.jsx
@@ -1,10 +1,20 @@
-export const MobileMenu = ({ menuOpen, setMenuOpen, toggleDarkMode }) => {
+export const MobileMenu = ({
+  menuOpen,
+  setMenuOpen,
+  isDark,
+  toggleDarkMode,
+}) => {
   const links = [
     { id: "home", label: "Home" },
     { id: "about", label: "About" },
     { id: "projects", label: "Projects" },
     { id: "contact", label: "Contact" },
   ];
+
+  const handleDarkMode = () => {
+    toggleDarkMode();
+    setMenuOpen(false);
+  };
 
   return (
     <div
@@ -17,24 +27,42 @@ export const MobileMenu = ({ menuOpen, setMenuOpen, toggleDarkMode }) => {
       <div className="absolute top-0 mt-5 flex items-center w-full justify-between px-10">
         <button
           className="text-gray-100 text-3xl focus:outline-none cursor-pointer"
-          onClick={toggleDarkMode}
+          onClick={handleDarkMode}
           id="darkMode"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M8 12a4 4 0 1 0 8 0a4 4 0 1 0-8 0m-5 0h1m8-9v1m8 8h1m-9 8v1M5.6 5.6l.7.7m12.1-.7l-.7.7m0 11.4l.7.7m-12.1-.7l-.7.7"
-            />
-          </svg>
+          {!isDark ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="0.90em"
+              height="0.90em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 12a4 4 0 1 0 8 0a4 4 0 1 0-8 0m-5 0h1m8-9v1m8 8h1m-9 8v1M5.6 5.6l.7.7m12.1-.7l-.7.7m0 11.4l.7.7m-12.1-.7l-.7.7"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="0.90em"
+              height="0.90em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992z"
+              />
+            </svg>
+          )}
         </button>
         <button
           aria-label="Close menu"
