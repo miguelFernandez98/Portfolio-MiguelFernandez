a6d648dac7e980531ddeda3931c629639108d252
Miguel Fernandez <139988212+miguelFernandez98@users.noreply.github.com>
2025-06-20 13:32:59 -0400

    Refactor code structure for improved readability and maintainability

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
-import { DarkModeProvider } from "./context/DarkModeProvider.jsx";
+import { DarkModeProvider } from "../src/context/DarkModeProvider.jsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DarkModeProvider>
       <App />
+      <Analytics /> {/* Vercel Analytics */}
     </DarkModeProvider>
   </StrictMode>
 );
